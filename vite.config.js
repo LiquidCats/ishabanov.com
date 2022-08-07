9c469d44f3a77b62641fe39556b663a073a0499c
Ilya Shabanov <i.s.shabanov@ya.ru>
2022-08-07 18:16:24 +0200

    feature: base config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,20 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
+            buildDirectory: "static",
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                'resources/scss/styles.scss',
+                'resources/typescript/scripts.ts',
             ],
             refresh: true,
         }),
     ],
+    // build: {
+    //     rollupOptions: {
+    //         output: {
+    //             entryFileNames: `assets/[name].js`,
+    //             assetFileNames: `assets/[name].[ext]`
+    //         }
+    //     }
+    // }
 });
