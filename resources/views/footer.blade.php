1bbfa54dff3d1d60b08b25db5e94a6a87c5950a1
Ilya Shabanov <i.s.shabanov@ya.ru>
2022-08-15 23:03:44 +0200

    refactor(views): footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,7 @@
+<footer id="footer" class="container">
+    <div class="row">
+        <div class="col">
+            <div class="text-center text-md-end text-muted my-4">&copy; LiquidCats</div>
+        </div>
+    </div>
+</footer>
