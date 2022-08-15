078b87c1ff2902434c5339f574615efa043f0d67
Ilya Shabanov <i.s.shabanov@ya.ru>
2022-08-15 23:04:11 +0200

    feat(views): hero

---
+++ resources/views/hero/index.blade.php
@@ -0,0 +1,39 @@
+<section id="hero" class="container-fluid vstack d-flex align-items-center justify-content-center">
+
+    <div class="lc-hero-background">
+        <svg width="1408" height="586" viewBox="0 0 1408 586" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <ellipse id="red" cx="722.917" cy="396.986" rx="189.014" ry="444.56" transform="rotate(90 722.917 396.986)" fill="#4274FF"/>
+            <ellipse id="cian" cx="963.44" cy="189.014" rx="189.014" ry="444.56" transform="rotate(90 963.44 189.014)" fill="#05EAFF"/>
+            <ellipse id="blue" cx="444.56" cy="189.014" rx="189.014" ry="444.56" transform="rotate(90 444.56 189.014)" fill="#FF05B9"/>
+        </svg>
+    </div>
+    <div class="row justify-content-center min-vh-100">
+        <div class="col-md-12 col-lg align-self-center">
+            <div class="display-1 m-0 fw-bold text-center text-lg-end">SOFTWARE DEVELOPER</div>
+            <div class="display-3 m-0 mb-4 fw-light text-center text-lg-end">WHO SOLVING PROBLEMS</div>
+            <div class="vstack gap-4 gap-lg-3 justify-content-end">
+                <div class="hstack gap-3 justify-content-lg-end justify-content-center">
+                    <div class="text-nowrap lc-component-chips">PHP: Senior</div>
+                    <div class="text-nowrap lc-component-chips">Go Lang: Middle</div>
+                    <div class="text-nowrap lc-component-chips">NodeJS: Middle+</div>
+                    <div class="text-nowrap lc-component-chips">JavaScript: Middle+</div>
+                </div>
+                <div class="hstack gap-3 justify-content-lg-end justify-content-center">
+                    <div class="lc-component-chips">Laravel: Senior</div>
+                    <div class="lc-component-chips">Symfony: Middle</div>
+                    <div class="lc-component-chips">React: Middle</div>
+                    <div class="lc-component-chips">Vue: Middle</div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-12 col-lg-auto align-self-center">
+            <img src="{{ asset("images/hero/person.jpg") }}" class="d-block img-fluid my-4 my-lg-2 mx-auto lc-hero-person" alt="LiquidCats - Person">
+        </div>
+{{--        <div class="col-12 d-none d-lg-block align-self-end justify-content-center text-center">--}}
+{{--            <a href="#experience" class="d-inline-block mt-3 mb-3 lc-hero-chevron">--}}
+{{--                <img src="{{ asset("images/hero/chevron.svg") }}" class="d-block mx-auto" alt="Chevron">--}}
+{{--            </a>--}}
+{{--        </div>--}}
+    </div>
+</section>
