7a5e51d5a27bba47fd401a0f2889eb5ea3b4b640
Ilya Shabanov <i.s.shabanov@ya.ru>
2022-08-15 23:04:49 +0200

    feat(views): contacts

---
+++ resources/views/contacts/info.blade.php
@@ -0,0 +1,4 @@
+<div class="lc-contacts-info">
+    <img src="{{ asset("images/contacts/info.svg") }}" alt="Info" class="d-block mx-auto img-fluid" width="300">
+    <div class="display-6 mt-4 text-center">Contact Info</div>
+</div>
