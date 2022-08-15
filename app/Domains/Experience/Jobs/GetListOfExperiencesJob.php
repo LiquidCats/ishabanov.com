35c87632ee9ab542f06b9ff3712dd5bcdfb25866
Ilya Shabanov <i.s.shabanov@ya.ru>
2022-08-15 23:12:38 +0200

    feat(domain): add base jobs

---
+++ app/Domains/Experience/Jobs/GetListOfExperiencesJob.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domains\Experience\Jobs;
+
+use App\Data\Models\Experience;
+use Illuminate\Support\Collection;
+use Lucid\Units\Job;
+
+/**
+ * Class GetListOfExperiencesJob.
+ */
+class GetListOfExperiencesJob extends Job
+{
+    /**
+     * @return Collection<int, Collection>
+     */
+    public function handle(): Collection
+    {
+        /* @var Collection<int, Experience> $experiences */
+        return Experience::query()
+            ->orderByDesc('started_at')
+            ->take(6)
+            ->get();
+    }
+}
