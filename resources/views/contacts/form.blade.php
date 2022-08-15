b6e2c05932f3b73c59794b98c165b0cb827513bc
Ilya Shabanov <i.s.shabanov@ya.ru>
2022-08-15 23:04:49 +0200

    refactor(views): contacts

---
+++ resources/views/contacts/form.blade.php
@@ -0,0 +1,46 @@
+<form action="{{ url('feedback') }}" method="post" class="bg-body p-5 lc-contacts-form">
+    <fieldset>
+        <legend></legend>
+
+        <div class="grid gap-3">
+            <div class="g-col-6">
+                <label for="exampleInputEmail1" class="form-label small">Email address</label>
+                <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="g-col-6">
+                <label for="exampleInputEmail2" class="form-label small">Email address</label>
+                <input type="email" class="form-control form-control-lg" id="exampleInputEmail2" aria-describedby="emailHelp">
+            </div>
+
+            <div class="g-col-12">
+                <label for="exampleFormControlTextarea1" class="form-label small">Example textarea</label>
+                <textarea class="form-control form-control-lg" id="exampleFormControlTextarea1" rows="3"></textarea>
+            </div>
+        </div>
+
+        <div class="row text-end mt-3">
+            <div class="col">
+                <select id="exampleInputEmail3" class="form-select form-select-lg">
+                    <option value="1" disabled selected>Subject</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                </select>
+            </div>
+            <div class="col-auto">
+                <button type="submit" class="btn btn-outline-primary btn-lg">
+                    <span class="d-inline-flex justify-content-center align-items-center gap-2 text-uppercase">
+                        <span class="fs-6">Send</span>
+                        <i data-feather="arrow-right-circle"
+                           stroke-width="1.5"
+                           width="24"
+                           height="24"></i>
+                    </span>
+
+                </button>
+            </div>
+
+        </div>
+
+    </fieldset>
+</form>
---
+++ resources/views/contacts/links.blade.php
@@ -0,0 +1,38 @@
+<div class="d-flex gap-2 justify-content-center text-center lc-contacts-links mt-2">
+    <div>
+        <a href="https://www.linkedin.com/in/ilya-shabanov-17aba7154/"
+           target="_blank"
+           title="LinkedIn"
+           class="d-flex gap-2 align-items-center justify-content-center text-decoration-none p-2 lc-contacts-links-linkedin">
+            <i data-feather="linkedin"
+               stroke-width="1"
+               width="24"
+               height="24"></i>
+        </a>
+    </div>
+
+    <div>
+        <a href="https://www.linkedin.com/in/ilya-shabanov-17aba7154/"
+           target="_blank"
+           title="Instagram"
+           class="d-flex gap-2 align-items-center justify-content-center text-decoration-none p-2 lc-contacts-links-instagram">
+            <i data-feather="instagram"
+               stroke-width="1"
+               width="24"
+               height="24"></i>
+        </a>
+    </div>
+
+    <div>
+        <a href="https://www.linkedin.com/in/ilya-shabanov-17aba7154/"
+           target="_blank"
+           title="Telegram"
+           class="d-flex gap-2 align-items-center justify-content-center text-decoration-none p-2 lc-contacts-links-telegram">
+            <i data-feather="send"
+               stroke-width="1"
+               width="24"
+               height="24"></i>
+        </a>
+    </div>
+
+</div>
