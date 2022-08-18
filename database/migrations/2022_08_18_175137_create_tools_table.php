9dd1531a557735875106660748ae2915567cc884
Ilya Shabanov <i.s.shabanov@ya.ru>
2022-08-18 22:43:50 +0200

    feature: add migrations

---
+++ database/migrations/2022_08_18_175137_create_tools_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tools', static function (Blueprint $table) {
+            $table->string('id', 15)->primary();
+            $table->string('name', 15);
+            $table->string('type', 15)->nullable();
+            $table->string('level', 15)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tools');
+    }
+};
---
+++ database/migrations/2022_08_18_182357_create_experience_tool_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('experience_tool', static function (Blueprint $table) {
+            $table->integer('experience_id')
+                ->primary();
+            $table->integer('tool_id');
+            $table->tinyInteger('level_id')
+                ->default(0);
+
+            $table->foreign('experience_id')
+                ->references('id')
+                ->on('experiences')
+                ->cascadeOnDelete();
+
+            $table->foreign('tool_id')
+                ->references('id')
+                ->on('tools')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('experience_tool');
+    }
+};
