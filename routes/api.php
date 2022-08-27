1c5a692b52d7ba6d3aab78cc437a4e61c09d4bce
Ilya Shabanov <ishabanov@liquid-cats.com>
2022-08-27 15:01:30 +0200

    fix: code style

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\Api\UserFeedbackController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('feedback', UserFeedbackController::class);
+Route::middleware('throttle:3,60')
+    ->post('feedback', UserFeedbackController::class)
+    ->name('api.feedback');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Http\Controllers\HomepageController;
 use Illuminate\Support\Facades\Route;
 
