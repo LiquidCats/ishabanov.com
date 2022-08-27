b9831ccf53ddb67f2844c1b1ad025dd4fda2be3e
Ilya Shabanov <ishabanov@liquid-cats.com>
2022-08-27 15:49:20 +0200

    feature: bind repository and service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,19 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use App\Data\Contracts\Repositories\TelegramRepositoryContract;
+use App\Data\Repositories\TelegramRepository;
+use App\Data\ValueObject\Telegram\{
+    ChatId,
+    Token
+};
+use App\Domains\Feedback\Contracts\Services\FeedbackServiceContract;
+use App\Domains\Feedback\Services\FeedbackService;
+use Illuminate\Contracts\Config\Repository;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,8 +23,31 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
+        // Repositories
+        $this->app->singleton(TelegramRepositoryContract::class, static function (Application $app) {
+            /** @var Repository $config */
+            $config = $app->make('config');
+
+            $token = new Token($config->get('services.telegram.announcer.token'));
+            $api = $config->get('services.telegram.api');
+
+            return new TelegramRepository($token, $api);
+        });
+
+        // Domain.Feedback
+        $this->app->singleton(FeedbackServiceContract::class, static function (Application $app) {
+            /** @var Repository $config */
+            $config = $app->make('config');
+
+            /** @var TelegramRepositoryContract $repository */
+            $repository = $app->make(TelegramRepositoryContract::class);
+
+            $chatId = new ChatId($config->get('services.telegram.announcer.chat_id'));
+
+            return new FeedbackService( $app, $repository, $chatId);
+        });
     }
 
     /**
