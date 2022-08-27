37fa9eecabf80ca73d6fda856451231ec59705db
Ilya Shabanov <ishabanov@liquid-cats.com>
2022-08-27 15:32:24 +0200

    feature: feedback mail

---
+++ app/Mail/FeedbackReceived.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Mail;
+
+use App\Http\Requests\UserFeedbackRequest;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class FeedbackReceived extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    protected string $userName;
+    protected string $userEmail;
+    protected string $userMessage;
+    protected int $userType;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(UserFeedbackRequest $request)
+    {
+        $this->userName = $request->validated('name');
+        $this->userEmail = $request->validated('email');
+        $this->userMessage = $request->validated('message');
+        $this->userType = $request->validated('subject');
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build(): static
+    {
+        return $this
+            ->text('emails.feedback')
+            ->tag('feedback')
+            ->metadata('subject', $this->userType)
+            ->with([
+                'name' => $this->userName,
+                'email' => $this->userEmail,
+                'message' => $this->userMessage,
+                'subject' => $this->userType,
+            ]);
+    }
+}
