b4f20900dbd7743071a7120c4424e243099b36f9
Ilya Shabanov <ishabanov@liquid-cats.com>
2022-09-04 01:07:45 +0200

    refactor: update reverse proxy

--- deploy/production/run.sh
+++ deploy/production/run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 set -e
 
-docker compose -f docker-stack.yml build
-docker stack deploy -c docker-stack.yaml onepager
+### Deploy Reverse Proxy
+cp deploy/production/proxy/* /etc/nginx/sites-enabled/*
+nginx -s reload
+service nginx restart
+
+### Deploy Docker Stack
+docker stack deploy -c docker-stack.yml onepager
+
+###
