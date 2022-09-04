c80ce934e7a8a9b1a8629262999604275c756181
Ilya Shabanov <ishabanov@liquid-cats.com>
2022-09-04 18:10:05 +0200

    refactor: deploy

--- deploy/production/run.sh
+++ deploy/production/run.sh
@@ -6,7 +6,10 @@ cp deploy/production/proxy/* /etc/nginx/sites-enabled/*
 nginx -s reload
 service nginx restart
 
+### Setup Rules
+chmod -R 644 .
+chmod -R ug+rwx storage
+chmod -R ug+rwx bootstrap/cache
+
 ### Deploy Docker Stack
 docker stack deploy -c docker-stack.yml onepager
-
-###
