5813f1d1bc1324b456ccb0d44dcaa2928c03c994
Ilya Shabanov <ishabanov@liquid-cats.com>
2022-09-24 23:54:23 +0200

    fix: no prune

--- deploy/production/deploy.sh
+++ deploy/production/deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 ### Deploy Docker Stack
-docker stack deploy -c docker-stack.yml onepager --with-registry-auth --prune
+docker stack deploy -c docker-stack.yml onepager --with-registry-auth
 
 ### Force Port Forwarding
 docker service update onepager_app --quiet --detach --publish-add 8000:80
