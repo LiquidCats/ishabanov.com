08c08a6677a8153359c863485512b4c89d4d4219
Ilya Shabanov <ishabanov@liquid-cats.com>
2022-11-01 17:43:48 +0100

    refactor: change about yourself text

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -8,10 +8,9 @@
                 <div class="rounded-4 p-5">
                     <div>
                         <h2>About Me</h2>
-                        <p>Senior Software Engineer with a demonstrated history of working in the information technology
-                            and services industry. Skilled in PHP, JS,NodeJs, React, Vue.js, Laravel, Symphony, Zend
-                            Framework, CQRS, Event Sourcing and Payment Gateways for Blockchain architecture. Strong
-                            engineering professional with a bachelor focused in Programming Engineering.</p>
+                        <p>Hey! 👋 This is me 👆 and my name is Ilya. Already for many years, I am working as a Software Engineer. And the first thing you have to know about me I am mad about coding. From the very begging of my carrier I was concentrating on delivering optimized and highly maintainable features.
+                        <p>My geeky nature never let me stop improving my skills and constantly learning new stuff. For the past years, I tried myself as a developer in different domains such as remote education, music distribution, grocery delivery, and fintech. Filled my stack with different programming languages: PHP, Golang, NodeJS, JavaScript; frameworks: Laravel, React, Vue; Tools: Kafka, ElasticSearch, RabbitMQ, Redis, Doker, Docker Swarm, Kubernetes, etc.; and databases: MySQL, PostgreSQL, MSSQL</p>
+                        <p>Currently, all of my experience allows me to build high-end web applications with all modern technologies and approaches. </p>
                     </div>
                     <div class="text-muted fs-4 my-1">
                         Current experience: {{ $duration->getYears() }} years {{ $duration->getMonths() }} months
