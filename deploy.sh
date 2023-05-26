1a7d3762ad21ee578e434595192d1bbff2b63615
ilya.shabanov <ilya.shabanov@coinspaid.com>
2023-05-26 21:04:27 +0400

    check

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-chown $USER:www-data -R .
+sudo chown $USER:www-data -R .
 
-chmod 755 -R .
+sudo chmod 775 -R .
 
 rm /var/www/www.liquid-cats.com
 ln -s $(pwd) /var/www/www.liquid-cats.com
