df7fd5ca07b5a7d8151f44e4402580677854efe9
ilya.shabanov <ilya.shabanov@coinspaid.com>
2023-05-26 21:08:00 +0400

    finalize

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,10 @@ sudo chown $USER:www-data -R .
 
 sudo chmod 775 -R .
 
+php artisan config:cache
+php artisan event:cache
+php artisan route:cache
+php artisan view:cache
+
 rm /var/www/www.liquid-cats.com
 ln -s $(pwd) /var/www/www.liquid-cats.com
