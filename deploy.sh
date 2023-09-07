6a8f3604e986c94e139a2b4430db385c10baba97
ilya.shabanov <ishabanov@liquid-cats.com>
2023-09-07 17:59:26 +0300

    fix: critical issues

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ php artisan event:cache
 php artisan route:cache
 php artisan view:cache
 
-rm /var/www/www.liquid-cats.com
-ln -s $(pwd) /var/www/www.liquid-cats.com
+rm /var/www/ishabanov.com
+ln -s $(pwd) /var/www/ishabanov.com
