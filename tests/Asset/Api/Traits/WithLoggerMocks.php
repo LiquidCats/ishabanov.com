49dca47853fd766d0e3b19978bfb5ce5fa01dc54
ilya.shabanov <ishabanov@liquid-cats.com>
2023-09-09 23:45:15 +0300

    feature: add test assets

---
+++ tests/Asset/Api/Traits/WithLoggerMocks.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Asset\Api\Traits;
+
+use Illuminate\Log\LogManager;
+use Psr\Log\LoggerInterface;
+
+trait WithLoggerMocks
+{
+
+    protected function mockFeedbackLogger(): void
+    {
+        $logMock = \Mockery::mock(LoggerInterface::class);
+        $logMock->shouldReceive('notice')
+            ->withAnyArgs()
+            ->andReturn()
+            ->once();
+
+        $managerMock = \Mockery::mock(LogManager::class);
+        $managerMock->shouldReceive('channel')
+            ->withArgs(['feedback'])
+            ->andReturn($logMock);
+
+        $this->app->instance(LogManager::class, $managerMock);
+    }
+}
