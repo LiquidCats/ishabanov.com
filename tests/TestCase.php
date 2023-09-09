bd2cfa24155f7ddcac1a3fdf373795703a281ff9
ilya.shabanov <ishabanov@liquid-cats.com>
2023-09-09 23:45:42 +0300

    feature: always seed in tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,10 +4,13 @@ declare(strict_types=1);
 
 namespace Tests;
 
-use Illuminate\Foundation\Testing\{DatabaseMigrations, TestCase as BaseTestCase};
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
     use DatabaseMigrations;
+
+    protected bool $seed = true;
 }
