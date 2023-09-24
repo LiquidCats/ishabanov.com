402867bdd2766ed7186162fd3a753e1fc47ba0df
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-09-24 23:28:56 +0400

    chore(blog): add route names

--- src/Services/Pages/routes/web.php
+++ src/Services/Pages/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Pages\Presentation\Http\Controllers\BlogController;
 use App\Pages\Presentation\Http\Controllers\HomepageController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomepageController::class);
+Route::get('/', HomepageController::class)->name('pages.home');
+Route::get('blog', BlogController::class)->name('pages.blog');
