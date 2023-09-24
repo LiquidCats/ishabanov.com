dfd2d8a055bb98936b4ea4b7a84450245b968778
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-09-24 23:30:08 +0400

    chore(blog): add request to page composer

--- src/Domains/Kernel/Contracts/Services/PageComposerServiceContract.php
+++ src/Domains/Kernel/Contracts/Services/PageComposerServiceContract.php
@@ -3,8 +3,9 @@
 namespace App\Domains\Kernel\Contracts\Services;
 
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 
 interface PageComposerServiceContract
 {
-    public function view(): View;
+    public function view(Request $request): View;
 }
