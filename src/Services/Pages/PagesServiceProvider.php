1afa70ffa8444d431f939f037b100631b3b6cacd
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-09-24 23:30:46 +0400

    chore(blog): rebind

--- src/Services/Pages/PagesServiceProvider.php
+++ src/Services/Pages/PagesServiceProvider.php
@@ -3,11 +3,18 @@
 namespace App\Pages;
 
 use App\Data\Database\Eloquent\Repositories\ExperienceRepository;
+use App\Data\Database\Eloquent\Repositories\PostRepository;
+use App\Domains\Blog\Contracts\Repositories\PostRepositoryContract;
 use App\Domains\Homepage\Contracts\Repositories\ExperienceRepositoryContract;
 use App\Domains\Kernel\Contracts\Services\PageComposerServiceContract;
+use App\Pages\Application\Services\BlogService;
 use App\Pages\Application\Services\HomepageService;
+use App\Pages\Presentation\Http\Controllers\BlogController;
+use App\Pages\Presentation\Http\Controllers\HomepageController;
+use App\Pages\Presentation\Http\Views\Components\Navbar;
 use App\Pages\Provides\RouteServiceProvider;
 use Carbon\Laravel\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
 
 class PagesServiceProvider extends ServiceProvider
 {
@@ -15,7 +22,18 @@ class PagesServiceProvider extends ServiceProvider
     {
         $this->app->register(RouteServiceProvider::class);
         //
-        $this->app->singleton(PageComposerServiceContract::class, HomepageService::class);
+        $this->app->when(HomepageController::class)
+            ->needs(PageComposerServiceContract::class)
+            ->give(HomepageService::class);
+        $this->app->when(BlogController::class)
+            ->needs(PageComposerServiceContract::class)
+            ->give(BlogService::class);
         $this->app->singleton(ExperienceRepositoryContract::class, ExperienceRepository::class);
+        $this->app->singleton(PostRepositoryContract::class, PostRepository::class);
+    }
+
+    public function boot(): void
+    {
+        Blade::component('navbar', Navbar::class);
     }
 }
