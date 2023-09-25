1d708d3083c3a4fedcd68e11cdec1553857c85a7
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-09-25 23:54:42 +0400

    chore(auth): simple auth

---
+++ src/Services/Authz/Presentation/Http/Controllers/SignInController.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Authz\Presentation\Http\Controllers;
+
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Auth;
+use function redirect;
+use function route;
+
+class SignInController extends Controller
+{
+    public function __invoke(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::check()) {
+            return redirect()->intended(route('admin.dashboard'));
+        }
+
+        if (Auth::attempt($credentials, true)) {
+            $request->session()->regenerate();
+            //
+            return redirect()->intended(route('admin.dashboard'));
+        }
+
+        return back()
+            ->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+            ])
+            ->onlyInput('email');
+    }
+}
