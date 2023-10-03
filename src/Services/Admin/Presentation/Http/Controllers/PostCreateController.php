637c4f0c45d6694ff1a5318dd6eb9b4ddf4a06cd
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-10-03 17:00:06 +0400

    feature: add create page

---
+++ src/Services/Admin/Presentation/Http/Controllers/PostCreateController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Admin\Presentation\Http\Controllers;
+
+use App\Data\Database\Eloquent\Models\Tag;
+use App\Domains\Kernel\Contracts\Services\PageComposerServiceContract;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use function compact;
+use function view;
+
+class PostCreateController extends Controller
+{
+    public function __construct(private readonly PageComposerServiceContract $service)
+    {
+    }
+
+    public function __invoke(Request $request): View
+    {
+        return $this->service->view($request);
+    }
+}
