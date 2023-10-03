234b489a07f47f42ea3cca66220590af085453b4
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-10-03 12:54:59 +0400

    chore(posts): routing

--- src/Services/Admin/Provides/RouteServiceProvider.php
+++ src/Services/Admin/Provides/RouteServiceProvider.php
@@ -10,8 +10,14 @@ class RouteServiceProvider extends BaseRouteServiceProvider
     public function boot(): void
     {
         $this->routes(function () {
-            Route::middleware('web')
+            Route::middleware(['web', 'auth'])
+                ->prefix('admin')
                 ->group(__DIR__.'/../routes/web.php');
+
+            Route::middleware(['api', 'auth:sanctum'])
+                ->prefix('admin')
+                ->prefix('api')
+                ->group(__DIR__.'/../routes/api.php');
         });
     }
 }
--- src/Services/Admin/routes/web.php
+++ src/Services/Admin/routes/web.php
@@ -3,15 +3,27 @@
 declare(strict_types=1);
 
 use App\Admin\Presentation\Http\Controllers\DashboardController;
+use App\Admin\Presentation\Http\Controllers\PostCreateController;
 use App\Admin\Presentation\Http\Controllers\PostListController;
+use App\Admin\Presentation\Http\Controllers\PostStoreController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth')
-    ->prefix('admin')
+Route::get('/', fn () => redirect(route('admin.dashboard')));
+Route::get('dashboard', DashboardController::class)->name('admin.dashboard');
+
+Route::prefix('posts')
     ->group(static function () {
-        Route::get('/', fn () => redirect(route('admin.dashboard')));
         //
-        Route::get('dashboard', DashboardController::class)->name('admin.dashboard');
-        Route::get('posts', PostListController::class)->name('admin.posts.list');
-        Route::get('posts/{post_id}', PostListController::class)->name('admin.posts.edit');
+        // POSTS
+        // view
+        Route::get('/', PostListController::class)->name('admin.posts.list');
+        Route::get('create', PostCreateController::class)->name('admin.posts.create');
+        Route::get('edit/{post_id}', PostListController::class)->name('admin.posts.edit');
+
+        // handle
+        Route::post('store', PostStoreController::class)->name('admin.posts.store');
+        Route::patch('state/{post_id}', fn () => redirect(route('admin.posts.list')))->name('admin.posts.state');
+        Route::delete('delete/{post_id}', fn () => redirect(route('admin.posts.list')))->name('admin.posts.delete');
+        // TAGS
+        // FILES
     });
