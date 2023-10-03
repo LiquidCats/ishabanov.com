590f1d8a930aa77fecffeafb9421db4358aa8b06
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-10-03 17:00:57 +0400

    refactor: remove unused

--- src/Domains/Blog/Contracts/Repositories/TagRepositoryContract.php
+++ src/Domains/Blog/Contracts/Repositories/TagRepositoryContract.php
@@ -3,17 +3,20 @@
 namespace App\Domains\Blog\Contracts\Repositories;
 
 use App\Data\Database\Eloquent\Models\Tag;
-use App\Domains\Blog\ValueObjects\PostId;
 use App\Domains\Blog\ValueObjects\TagId;
 use Illuminate\Support\Collection;
 
 interface TagRepositoryContract
 {
+    /**
+     * @return Collection<int, Tag>
+     */
+    public function getAll(): Collection;
+
     public function create(string $name): Tag;
 
-    public function search(string $search): Collection;
-
-    public function unlinkFromPost(PostId $postId, TagId $tagId): bool;
-
-    public function linkToPost(PostId $postId, TagId $tagId): bool;
+    /**
+     * @param  TagId<int>  $tagId
+     */
+    public function delete(TagId $tagId): bool;
 }
