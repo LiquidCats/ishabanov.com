b300308f915a462e6b7b5e0a04793de79c861355
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-10-03 16:23:32 +0400

    chore(admin): dashboard

--- src/Services/Admin/Presentation/Http/Controllers/DashboardController.php
+++ src/Services/Admin/Presentation/Http/Controllers/DashboardController.php
@@ -4,13 +4,20 @@ declare(strict_types=1);
 
 namespace App\Admin\Presentation\Http\Controllers;
 
+use App\Domains\Kernel\Contracts\Services\PageComposerServiceContract;
+use Illuminate\Contracts\View\View;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use function view;
 
 class DashboardController extends Controller
 {
-    public function __invoke()
+    public function __construct(private readonly PageComposerServiceContract $service)
     {
-        return view('admin.pages.dashboard.index');
+    }
+
+    public function __invoke(Request $request): View
+    {
+        return $this->service->view($request);
     }
 }
