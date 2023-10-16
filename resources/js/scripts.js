98077d141427c883850f6f7fbddc87f4dbf5b6ab
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-10-16 19:30:19 +0400

    fix(blog): adjust assets

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,10 +1,10 @@
-import * as Bootstrap from 'bootstrap'
-import Feedback from "./sections/feedback";
+// import 'bootstrap'
+// import Feedback from "./sections/feedback";
 
-document.addEventListener('DOMContentLoaded', function () {
-    const feedback = Feedback.capture();
-    feedback.disableSubmit(true)
-    feedback.addCounter('message')
-})
+// document.addEventListener('DOMContentLoaded', function () {
+//     const feedback = Feedback.capture();
+//     feedback.disableSubmit(true)
+//     feedback.addCounter('message')
+// })
 
 
