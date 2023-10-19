2453120e88629a45b848b0e1849f63d534f7c7c6
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-10-19 20:59:02 +0400

    fix: remove full text fields (for now)

--- database/migrations/2023_09_12_191630_create_tags_table.php
+++ database/migrations/2023_09_12_191630_create_tags_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('slug', 255)->unique();
             $table->timestamps();
 
-            $table->fullText(['name', 'slug']);
+            // $table->fullText(['name', 'slug']);
         });
     }
 
