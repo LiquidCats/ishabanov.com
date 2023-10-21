b88265313e44e7373beed7461c170322ff5fc99e
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-10-21 22:52:46 +0400

    fix: security

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        (new UserSeeder())->run();
         (new ToolSeeder())->run();
         (new ExperienceSeeder())->run();
         (new ExperienceToolSeeder())->run();
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -6,11 +6,15 @@ namespace Database\Seeders;
 
 use App\Data\Database\Eloquent\Models\User;
 use Illuminate\Database\Seeder;
+use function app;
 
 class UserSeeder extends Seeder
 {
     public function run(): void
     {
+        if (app()->environment('production')) {
+            return;
+        }
         $user = User::find(1);
         if ($user === null) {
             User::factory()->create();
