63891fad14cc37de7d3db072e1950c79e0e7c864
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-10-25 20:29:31 +0400

    chore: final fixes

--- resources/views/pages/blog/list.blade.php
+++ resources/views/pages/blog/list.blade.php
@@ -8,11 +8,12 @@
         <div class="row mt-2 mb-5 g-4">
             @if($posts->isEmpty())
                 <div class="col-12 text-center">
-                    <img src="{{ asset('images/void.svg') }}" alt="nothing" class="img-fluid mx-auto d-block" style="max-width: 320px">
-                    <div class="mt-5">
-                        <div class="fs-1">I haven't written anything yet</div>
+                    <div class="mt-2 mb-5">
                         <div class="fs-1">¯\_(ツ)_/¯</div>
+                        <div class="fs-1">I haven't written anything yet</div>
                     </div>
+                    <img src="{{ asset('images/void.svg') }}" alt="nothing" class="img-fluid mx-auto d-block" style="max-height: 320px">
+
                     <div class="mt-3">
                         <a href="{{ back()->getTargetUrl() }}" class="btn btn-lg btn-outline-primary">
                             <i class="bi bi-chevron-left"></i>
