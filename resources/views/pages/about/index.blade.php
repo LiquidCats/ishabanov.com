1c0ec0f847e56549fc65e93b714f7b3a235199c3
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-10-25 19:39:05 +0400

    chore: update information about me

--- resources/views/pages/about/index.blade.php
+++ resources/views/pages/about/index.blade.php
@@ -14,27 +14,27 @@
                 <div class="rounded-4 p-5">
                     <div>
                         <h1>About Me</h1>
+                        <div class="fs-5 mt-2 mb-2">Current experience: {{ $duration }}</div>
 
-                        <p>I am Ilya, a passionate Software Engineer who has been working in the industry for several
-                            years. Coding is not just a job for me, it's a way of life. I have always been focused on
-                            delivering optimized and highly maintainable features to my clients.</p>
+                        <p>Hey! 👋 I'm Ilya. For many years now, I've been immersing myself in the world of Software
+                            Engineering. First thing to note? Coding isn't just a profession for me—it's a passion.
+                            Since the outset of my journey, I've honed in on delivering features that are not only
+                            optimized but also highly maintainable.</p>
 
-                        <p>As a self-proclaimed geek, I'm always looking to improve my skills and stay up-to-date with
-                            the latest technologies. Throughout my career, I have worked in various domains such as
-                            remote education, music distribution, grocery delivery, and fintech. I have gained
-                            experience in a variety of programming languages including PHP, Golang, NodeJS, and
-                            JavaScript, as well as frameworks like Laravel, React, and Vue. I am also proficient in
-                            working with tools such as Kafka, ElasticSearch, RabbitMQ, Redis, Docker, Docker Swarm,
-                            Kubernetes, and databases including MySQL, PostgreSQL, and MSSQL.</p>
+                        <p>My inner geek never rests. I'm always on the prowl to amplify my expertise and stay synced
+                            with the latest tech vibes. Over the years, I've dipped my toes into a myriad of domains,
+                            from remote education and music distribution to grocery delivery and fintech. My tech
+                            arsenal? It's brimming with languages like PHP, Golang, NodeJS, and JavaScript; frameworks
+                            including Laravel, React, and Vue; and an array of tools like Kafka, ElasticSearch,
+                            RabbitMQ, Redis, Docker, Docker Swarm, and Kubernetes. Let's not forget databases like
+                            MySQL, PostgreSQL, and MSSQL.</p>
 
-                        <p>My experience and knowledge enable me to build high-end web applications using modern
-                            technologies and approaches. I'm always excited to take on new challenges and help bring
-                            ideas to life. Feel free to explore my website and get in touch if you have any questions or
-                            projects you'd like to discuss. Let's build something great together!</p>
+                        <p>With all the experience under my belt, I'm primed to craft top-notch web applications infused
+                            with modern-day flair. Always game for fresh adventures in code and eager to breathe life
+                            into innovative ideas. Dive into my website, reach out with any queries or dream projects.
+                            Let's code the future together!</p>
                     </div>
-                    <div class="fs-5 my-1">
-                        Current experience: {{ $duration }}
-                    </div>
+
                 </div>
             </div>
         </div>
