20a43031e60f29b4816862c70a09189efb5442dd
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-11-12 17:36:42 +0400

    chore(animations): improve

--- resources/views/themes/default/pages/home/hero.blade.php
+++ resources/views/themes/default/pages/home/hero.blade.php
@@ -1,17 +1,32 @@
-<section id="hero" class="hero d-flex justify-content-center align-items-center overflow-hidden shadow">
-    <div class="d-flex flex-column flex-sm-row justify-content-center align-items-center">
-        <div class="col col-lg-5 col-xl-7 d-flex align-items-center px-3">
-            <img src="{{ asset('images/hero/person.png') }}"
-                 class="d-block img-fluid mx-auto hero-person"
-                 alt="Ilya Shabanov - Software Engineer" data-animation="slide-right">
-        </div>
-        <div class="col col-lg-7 col-xl-5 text-white px-3 d-flex align-items-center">
-            <div data-animation="slide-left">
-               <h1 class="display-1 fw-bold lh-1 text-uppercase">Software Engineer</h1>
-                <p class="fs-4">Hey, I'm Ilya! I love coding and have been doing it for years across cool areas like music, fintech,
-                    and more. I know my way around tech stuff, from PHP and JavaScript to Docker and Kubernetes. Got a
-                    project or idea? Let's chat and make it happen!</p>
+<section id="hero" class="hero d-flex flex-column justify-content-center align-items-center overflow-hidden shadow py-5 px-3">
+    <div class="container-fluid">
+        <div class="row g-5">
+            <div class="col-sm-12 col-md-6 col-lg-4 d-flex justify-content-start align-items-center">
+                <div data-animation="slide-right" class="text-white">
+                    <h1 class="display-1 fw-bold lh-1 text-uppercase">Software Engineer</h1>
+                    <p class="fs-4 fw-light">Meet Ilya: Passionate Coder, Tech Maestro, and Innovator. From music to fintech,
+                        I've coded it all using PHP, JavaScript, Docker, Kubernetes, and more. Have a project or a
+                        groundbreaking idea? Let's connect and bring it to life!</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-6 col-lg-5 d-flex align-items-center justify-content-center">
+                <div data-animation="slide-up">
+                   <img src="{{ asset('images/hero/person.png') }}"
+                        class="d-block img-fluid mx-auto hero__person"
+                        alt="Ilya Shabanov - Software Engineer">
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-3 d-flex align-items-center">
+                <div class="rounded-4 text-white fs-2 fw-light" data-animation="slide-left">
+                    <p>Unveil the Secrets to Success: Explore My Exclusive Insights and Expert Tips!</p>
+                    <button class="btn btn-secondary btn-lg text-up">Learn More <i class="bi bi-chevron-right fw-bold"></i></button>
+                </div>
             </div>
         </div>
+
+    </div>
+
+    <div class="col-12 mt-5 mt-lg-auto mb-2 d-flex flex-column align-items-center">
+        <p class="fs-1 text-white fw-bolder m-0 opacity-50"><i class="bi bi-chevron-down"></i></p>
     </div>
 </section>
