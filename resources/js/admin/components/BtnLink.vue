606f02160072e98c93f293c03ad242c8b43b3ce8
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-11-24 23:25:39 +0400

    feature(admin): implement components

---
+++ resources/js/admin/components/BtnLink.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+    import type {Colors} from "../types/colors";
+
+    interface Props {
+        type: Colors,
+        icon?: string,
+        to?: string,
+    }
+
+    defineProps<Props>()
+</script>
+
+<template>
+    <router-link type="button" :to="to" :class="'btn btn-' + type">
+        <i v-if="icon" :class="'bi bi-' + icon"></i> <slot></slot>
+    </router-link>
+</template>
+
+<style scoped>
+
+</style>
