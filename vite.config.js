67395297d937bba39969ed1f68dac752ce6fc6ad
Ilya Shabanov <ishabanov@liquid-cats.com>
2023-11-24 23:22:20 +0400

    feature(frontend): add configs

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,34 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            "@/admin": "resources/js/admin"
+        }
+    },
     plugins: [
         laravel({
             buildDirectory: "static",
             input: [
                 'resources/scss/themes/default/styles.scss',
                 'resources/js/themes/default/scripts.js',
+                'resources/js/admin/index.ts',
             ],
-            refresh: true,
+            refresh: [
+                'lang/**',
+                'resources/views/**',
+                'src/Services/**',
+            ],
+        }),
+     vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
         }),
     ],
 });
