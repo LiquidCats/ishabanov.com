322010c5ca2ef45aa0b9fe9e1eaf4304fdb49298
ilya.shabanov <ishabanov@liquid-cats.com>
2023-11-26 16:55:26 +0400

    chore(js): split out page buttons

---
+++ resources/js/admin/components/PageButtons.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+
+import {useRouter} from "vue-router";
+//
+import Btn from "./Btn.vue";
+import {Colors} from "../types/colors";
+
+const router = useRouter()
+
+defineProps({
+    disable: {
+        default: false,
+        type: Boolean,
+        required: true
+    }
+})
+
+</script>
+
+<template>
+    <div class="mb-3 d-flex gap-2">
+        <Btn :type="Colors.dark" icon="arrow-left" @click="router.back()">Back</Btn>
+        <Btn :type="Colors.primary"
+             icon="floppy"
+             @click.prevent="$emit('click:save', $event)"
+             :disabled="disable"
+             :class="{'disabled': disable}">Save</Btn>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
