e30d9c576398ae87557af5e8529b35f6097a8379
ilya.shabanov <ishabanov@liquid-cats.com>
2023-12-01 22:13:33 +0400

    chore(admin): adjust file resource

--- src/Services/Admin/Presentation/Http/Resources/FileResource.php
+++ src/Services/Admin/Presentation/Http/Resources/FileResource.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace App\Admin\Presentation\Http\Resources;
 
 use App\Data\Database\Eloquent\Models\FileModel;
+use App\Domains\Files\Contracts\Repositories\UploadedFilesStorageContract;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
 
 use function asset;
 
@@ -20,10 +22,14 @@ class FileResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $path = Str::startsWith($this->resource->path, UploadedFilesStorageContract::PATH)
+            ? asset('storage/'.$this->resource->path)
+            : asset('storage/media/'.$this->resource->path);
+
         return [
             'hash' => $this->resource->hash,
             'type' => $this->resource->type,
-            'path' => asset('storage/'.$this->resource->path),
+            'path' => $path,
             'extension' => $this->resource->extension,
             'name' => $this->resource->name,
             'file_size' => $this->resource->file_size,
