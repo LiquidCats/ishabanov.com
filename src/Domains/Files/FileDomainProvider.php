1f35fcfb19370d7bd193aa2dafcaac43d2c44d7d
ilya.shabanov <ishabanov@liquid-cats.com>
2023-12-01 22:14:56 +0400

    chore(admin): adjust

--- src/Domains/Files/FileDomainProvider.php
+++ src/Domains/Files/FileDomainProvider.php
@@ -6,9 +6,9 @@ namespace App\Domains\Files;
 
 use App\Admin\Application\Services\FileService;
 use App\Data\Database\Eloquent\Models\FileModel;
-use App\Data\Filesystem\Storage\Repositories\StorageRepository;
+use App\Data\Filesystem\Storage\Repositories\UploadedFilesStorage;
 use App\Domains\Files\Contracts\Repositories\FileRepositoryContract;
-use App\Domains\Files\Contracts\Repositories\StorageRepositoryContract;
+use App\Domains\Files\Contracts\Repositories\UploadedFilesStorageContract;
 use App\Domains\Files\Contracts\Services\FileServiceContract;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Filesystem\Factory;
@@ -24,10 +24,10 @@ class FileDomainProvider extends ServiceProvider
 
     public function register(): void
     {
-        $this->app->singleton(StorageRepositoryContract::class, static function (Container $app) {
+        $this->app->singleton(UploadedFilesStorageContract::class, static function (Container $app) {
             /** @var Factory $f */
             $f = $app->make(Factory::class);
-            return new StorageRepository($f->disk('public'));
+            return new UploadedFilesStorage($f->disk('public'));
         });
         $this->app->singleton(FileRepositoryContract::class, FileModel::class);
         $this->app->singleton(FileServiceContract::class, FileService::class);
