ad73f12b6962f5a1725361598dc887a389e65a68
ilya.shabanov <ishabanov@liquid-cats.com>
2023-12-01 22:13:49 +0400

    chore(admin): improve validation

--- src/Services/Admin/Presentation/Http/Requests/PostStoreRequest.php
+++ src/Services/Admin/Presentation/Http/Requests/PostStoreRequest.php
@@ -17,30 +17,30 @@ class PostStoreRequest extends FormRequest
             'title' => ['required', 'string', 'min:5', 'max:250'],
             'preview' => ['required', 'string', 'min:5'],
             'content' => ['required', 'string', 'min:5'],
-            'is_draft' => ['sometimes', 'accepted'],
+            'is_draft' => ['sometimes', 'boolean'],
             'published_at' => ['date'],
-            'post_tags' => ['array'],
             'preview_image_id' => [
-                'sometimes',
+                'nullable',
                 'string',
                 Rule::when(
-                    $this->get('preview_image_id') !== 'none',
+                    $this->get('preview_image_id') !== null,
                     [
                         Rule::exists('files', 'hash'),
                     ],
                 ),
             ],
             'preview_image_type' => [
-                'sometimes',
+                'nullable',
                 'string',
                 Rule::when(
-                    $this->get('preview_image_type') !== 'none',
+                    $this->get('preview_image_type') !== null,
                     [
                         Rule::enum(PostPreviewType::class),
                     ],
                 ),
             ],
-            'post_tags.*' => ['numeric', Rule::exists(TagModel::class, 'id')],
+            'tags' => ['array'],
+            'tags.*.id' => ['numeric', Rule::exists(TagModel::class, 'id')],
         ];
     }
 }
