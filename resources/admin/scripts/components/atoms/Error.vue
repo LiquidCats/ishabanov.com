0c996e375fc404cf86d7b93aaf813567211aa5ad
Ilya Shabanov <ishabanov@liquid-cats.com>
2024-02-21 12:27:34 +0400

    fix

--- resources/admin/scripts/components/atoms/Error.vue
+++ resources/admin/scripts/components/atoms/Error.vue
@@ -13,7 +13,7 @@ const messages = computed(() => props.errors[props.name] || [])
 </script>
 
 <template>
-    <div class="alert alert-danger mt-1 p-1 small" role="alert" v-if="messages.length">
+    <div class="text-sm text-gray-50" role="alert" v-if="messages.length">
         <ul class="m-0">
             <li v-for="message in messages">{{ message }}</li>
         </ul>
