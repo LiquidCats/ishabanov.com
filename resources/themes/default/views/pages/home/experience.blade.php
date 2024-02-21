a73f24ab6ce53395e3c3e8c2e686faa155965663
Ilya Shabanov <ishabanov@liquid-cats.com>
2024-02-21 12:27:16 +0400

    chore: refactor index page

--- resources/themes/default/views/pages/home/experience.blade.php
+++ resources/themes/default/views/pages/home/experience.blade.php
@@ -1,45 +1,36 @@
 @php /** @var Illuminate\Support\Collection<int, App\Data\Database\Eloquent\Models\ExperienceModel> $experiences */ @endphp
-<section id="experience" class="d-flex experience position-relative shadow">
-    <div class="col-2 d-none d-md-block" style="background-color: #F5F5F5">
-        <div class="experience-timeline sticky-top min-vh-100 d-flex flex-column align-items-center justify-content-center">
-            @foreach($experiences as $experience)
-                <a href="#experience-{{ $experience?->started_at?->year }}"
-                   class="experience-timeline__year fs-3 fw-bold">{{ $experience?->started_at?->year }}</a>
-            @endforeach
-        </div>
-    </div>
-    <div class="experience-descriptions col bg-dark" tabindex="0">
-        @foreach($experiences as $experience)
-            <div id="experience-{{ $experience?->started_at?->year }}"
-                 class="experience-description min-vh-100 text-white d-flex justify-content-center align-items-center">
-                <div class="px-3 py-5" data-animation="squeeze-out">
-                    <div class="experience-description__image d-flex justify-content-start align-items-center bg-white mb-4 p-3">
-                        <img class="d-block mx-auto img-fluid" src="{{ asset($experience->company_logo) }}"
-                             alt="{{ $experience->company_name }}">
-                    </div>
-                    <div class="experience-description__content">
-                        <div class="experience-description__content__company fs-5 fw-light">
-                            <a href="{{ $experience->company_url }}"
-                               target="_blank"
-                               class="text-white text-decoration-none"><i
-                                        class="bi bi-link-45deg"></i> {{ $experience->company_name }}</a>
-                        </div>
-                        <h2 class="experience-description__content__position fw-bold mb-0">{{ $experience->position }}</h2>
-                        <div class="experience-description__content__years fs-5 fw-light">{{ $experience?->started_at?->year }}
-                            - {{ $experience?->ended_at?->year ?? 'now' }}</div>
-                        <div class="experience-description__content__tools mb-3 mt-3">
-                            @foreach($experience->tools as $tool)
-                                <span class="badge bg-light text-black fw-normal fs-6 mb-1">{{ $tool->name }}: {{ $tool?->pivot?->level_id?->getText() }}</span>
-                            @endforeach
-                        </div>
-                        <div class="experience-description__content__description fs-6">
-                            <p>{{ $experience->description }}</p>
-                        </div>
-                    </div>
+<section id="experience" class="container max-w-6xl mx-auto px-3 grid grid-cols-1 md:grid-cols-3 gap-x-3 gap-y-16 py-8 md:gap-y-48 md:py-48 overflow-clip">
+    @foreach($experiences as $experience)
+        <div id="experience-card" class="z-[2] md:col-span-2 grid grid-cols-1 relative">
+            <h2 class="text-gray-300 text-7xl md:text-8xl font-black">
+                {{$experience->company_name}}
+            </h2>
+            <div class="z-[1] bg-night p-6 rounded-xl relative">
+                <div class="mb-3">
+                    <h2 class="text-gray-50 text-5xl font-bold">{{ $experience->position }}</h2>
+                    <a href="{{ $experience->company_url }}"
+                       target="_blank"
+                       class="text-gray-300 inline-flex items-center gap-1 my-2 text-xl">
+                        <x-heroicon-o-arrow-top-right-on-square class="size-5"/>{{ $experience->company_name }}</a>
+                </div>
+                <div class="flex flex-wrap gap-1.5 mb-3">
+                    @foreach($experience->tools as $tool)
+                        <span class="px-4 p-1 border rounded-md border-stone-200 bg-stone-300 text-stone-700 text-sm">{{ $tool->name }}</span>
+                    @endforeach
+                </div>
+                <div class="text-lg text-white">
+                    <p>{!! $experience->description !!}</p>
                 </div>
             </div>
-        @endforeach
-
-    </div>
-
+        </div>
+        <div id="experience-logo" class="z-[1] md:flex md:flex-col md:justify-center md:items-center hidden">
+            <div class="flex items-center justify-center size-48 bg-white p-6 rounded-full mb-3 shadow-xl">
+                <img class="block"
+                     src="{{ asset($experience->company_logo) }}"
+                     alt="{{ $experience->company_name }}">
+            </div>
+            <span class="text-5xl text-stone-50 font-black leading-10">{{ $experience?->started_at?->year ?? now()->year }}</span>
+            <span class="text-5xl text-stone-50 font-black leading-10">{{ $experience?->ended_at?->year ?? now()->year }}</span>
+        </div>
+    @endforeach
 </section>
