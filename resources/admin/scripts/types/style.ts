fc1e629490ba3957d11c5d479af951764ae93b56
Ilya Shabanov <ishabanov@liquid-cats.com>
2024-04-06 12:54:22 +0400

    chore: add block types

---
+++ resources/admin/scripts/types/style.ts
@@ -0,0 +1,60 @@
+export enum BlockStyle {
+    // font family
+    FONT_FAMILY_SANS = 'sans',
+    FONT_FAMILY_SERIF = 'serif',
+    FONT_FAMILY_MONO = 'mono',
+    // font size
+    FONT_SIZE_XS = 'xs',
+    FONT_SIZE_SM = 'sm',
+    FONT_SIZE_BASE = 'base',
+    FONT_SIZE_LG = 'lg',
+    FONT_SIZE_XL = 'xl',
+    FONT_SIZE_XL2 = '2xl',
+    FONT_SIZE_XL3 = '3xl',
+    FONT_SIZE_XL4 = '4xl',
+    FONT_SIZE_XL5 = '5xl',
+    FONT_SIZE_XL6 = '6xl',
+    FONT_SIZE_XL7 = '7xl',
+    FONT_SIZE_XL8 = '8xl',
+    // font weight
+    FONT_WEIGHT_THIN = 'thin',
+    FONT_WEIGHT_EXTRALIGHT = 'extralight',
+    FONT_WEIGHT_LIGHT = 'light',
+    FONT_WEIGHT_NORMAL = 'normal',
+    FONT_WEIGHT_MEDIUM = 'medium',
+    FONT_WEIGHT_SEMIBOLD = 'semibold',
+    FONT_WEIGHT_BOLD = 'bold',
+    FONT_WEIGHT_EXTRABOLD = 'extrabold',
+    FONT_WEIGHT_BLACK = 'black',
+}
+
+export const fontFamilyStyles = [
+    BlockStyle.FONT_FAMILY_SANS,
+    BlockStyle.FONT_FAMILY_SERIF,
+    BlockStyle.FONT_FAMILY_MONO
+]
+export const fontSizeStyles = [
+    BlockStyle.FONT_SIZE_XS,
+    BlockStyle.FONT_SIZE_SM,
+    BlockStyle.FONT_SIZE_BASE,
+    BlockStyle.FONT_SIZE_LG,
+    BlockStyle.FONT_SIZE_XL,
+    BlockStyle.FONT_SIZE_XL2,
+    BlockStyle.FONT_SIZE_XL3,
+    BlockStyle.FONT_SIZE_XL4,
+    BlockStyle.FONT_SIZE_XL5,
+    BlockStyle.FONT_SIZE_XL6,
+    BlockStyle.FONT_SIZE_XL7,
+    BlockStyle.FONT_SIZE_XL8
+]
+export const fontWeightStyles = [
+    BlockStyle.FONT_WEIGHT_THIN,
+    BlockStyle.FONT_WEIGHT_EXTRALIGHT,
+    BlockStyle.FONT_WEIGHT_LIGHT,
+    BlockStyle.FONT_WEIGHT_NORMAL,
+    BlockStyle.FONT_WEIGHT_MEDIUM,
+    BlockStyle.FONT_WEIGHT_SEMIBOLD,
+    BlockStyle.FONT_WEIGHT_BOLD,
+    BlockStyle.FONT_WEIGHT_EXTRABOLD,
+    BlockStyle.FONT_WEIGHT_BLACK,
+]
