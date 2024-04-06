94e9670782091effc59062a67e661c8172876e32
Ilya Shabanov <ishabanov@liquid-cats.com>
2024-04-06 13:15:36 +0400

    chore: adjust input sizes

--- resources/admin/scripts/components/atoms/Form/FormField.vue
+++ resources/admin/scripts/components/atoms/Form/FormField.vue
@@ -25,7 +25,7 @@ const value = computed({
 </script>
 
 <template>
-    <input class="border text-lg rounded-md block w-full p-2.5 bg-stone-600 border-stone-500 placeholder-gray-400 text-white focus:ring-stone-300 focus:border-stone-300 outline-none duration-300"
+    <input class="border text-md rounded-md block w-full p-1.5 bg-stone-600 border-stone-500 placeholder-gray-400 text-white focus:ring-stone-300 focus:border-stone-300 outline-none duration-300"
            :class="{
                 'border-red-500': failed
             }"
