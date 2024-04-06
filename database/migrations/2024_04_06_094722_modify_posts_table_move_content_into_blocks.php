8eb5164e65a9562383c1d412756ff40d2d59536d
Ilya Shabanov <ishabanov@liquid-cats.com>
2024-04-06 17:22:13 +0400

    chore: add migrations for blocks column

---
+++ database/migrations/2024_04_06_094722_modify_posts_table_move_content_into_blocks.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Data\Database\Eloquent\Models\PostModel;
+use App\Domains\Blocks\Enums\BlockType;
+use App\Domains\Blocks\Renderers\RawRenderer;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::transaction(static function () {
+            /** @var Collection<int, PostModel> $posts */
+            $posts = PostModel::all();
+
+            foreach ($posts as $post) {
+                $block = RawRenderer::createAs(BlockType::RAW, [
+                    'content' => $post->content,
+                ]);
+
+                $post->blocks = Collection::make()->push($block);
+
+                $post->save();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        DB::transaction(static function () {
+            /** @var Collection<int, PostModel> $posts */
+            $posts = PostModel::all();
+
+            foreach ($posts as $post) {
+                /** @var RawRenderer $block */
+                $block = $post->blocks->first();
+
+                $post->content = $block->content;
+
+                $post->save();
+            }
+        });
+    }
+};
