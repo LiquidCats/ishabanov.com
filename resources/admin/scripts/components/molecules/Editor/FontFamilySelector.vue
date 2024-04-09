5a1dd133128eb5ea324d02ac774d9dbf8b272be8
Ilya Shabanov <ishabanov@liquid-cats.com>
2024-04-09 23:49:43 +0400

    chore: visuals

--- resources/admin/scripts/components/molecules/Editor/FontFamilySelector.vue
+++ resources/admin/scripts/components/molecules/Editor/FontFamilySelector.vue
@@ -33,14 +33,15 @@ const map = {
 </script>
 
 <template>
-    <div class="flex rounded-md overflow-clip border border-stone-500 divide-stone-700 divide-x">
+    <div class="flex rounded-md overflow-clip border border-stone-500 divide-stone-700 divide-x hover:border-stone-400 ease-in-out transition-colors">
         <button v-for="fontFamilyStyle in fontFamilyStyles"
                 :key="`font-family-${fontFamilyStyle}`"
-                class="bg-stone-600 hover:bg-stone-700 duration-300 ease-in-out px-4 py-1 uppercase text-sm"
+                class="bg-stone-600 hover:bg-stone-700 duration-300 ease-in-out transition-colors px-4 py-1 uppercase text-sm"
                 :class="{
                     'bg-stone-700': selected === fontFamilyStyle,
                     [map[fontFamilyStyle]]: true,
                 }"
+                :disabled="selected === fontFamilyStyle"
                 @click="selected = fontFamilyStyle">{{ fontFamilyStyle[0] }}</button>
     </div>
 </template>
