fa0e406c1c3e919206a2176257a5aa19d7ae0d18
Ilya Shabanov <ishabanov@liquid-cats.com>
2024-06-16 18:25:03 +0400

    feature: add animations

--- resources/themes/default/scripts/hero.js
+++ resources/themes/default/scripts/hero.js
@@ -1,26 +1,20 @@
-import {inView, stagger, timeline} from "motion";
+import {stagger, timeline} from "motion";
 
 document.addEventListener('DOMContentLoaded', () => {
     if (window.innerWidth <= 768) {
         return;
     }
 
-
-    const stop = inView(document.getElementById('hero'), ({target}) => {
-        timeline([
-            ["#hero:nth-child(1)", {y: [-100, 0], opacity: [0, 1]}, {duration: 1, delay: .1}],
-            ['#hero div > div', {y:[100, 0], opacity: [0, 1]}, {duration: 1, delay: stagger()}]
-        ], {
-            direction: 'alternate'
-        })
-    }, {
-        margin: "500px 0px"
-    })
-
+    const controls = timeline([
+        ["#hero #position", {y: [200, 200], scale: [.9, 1], opacity: [0, 1]}, {delay: 1, duration: 1}],
+        ["#hero #position", {y: [200, 0]}, {delay:.5, duration: .5,}],
+        ['#hero > #grid-blocks > div', {y:[100, 0], opacity: [0, 1]}, {duration: 1, delay: stagger()}],
+        ['nav', {y:[-100, 0], opacity: [0, 1]}, {duration: .3}],
+    ])
 
     document.addEventListener('resize', () => {
         if (window.innerWidth <= 768) {
-            stop()
+            controls.stop()
         }
     })
 })
