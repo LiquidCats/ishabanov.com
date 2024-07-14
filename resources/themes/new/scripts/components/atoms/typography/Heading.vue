d2d3549820e872f288eee76031daa508a22e73bd
ilya.shabanov <ishabanov@liquid-cats.com>
2024-07-14 15:38:49 +0400

    chore(new theme): add atoms

---
+++ resources/themes/new/scripts/components/atoms/typography/Heading.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {TextFamily, TextSize, TextWeight} from "../../../domain/types/components";
+import {computed} from "vue";
+// Props
+interface HeadingProps {
+    size?: TextSize;
+    family?: TextFamily;
+    weight?: TextWeight;
+    level?: 1 | 2 | 3 | 4 | 5 | 6;
+}
+// Define
+const props = withDefaults(defineProps<HeadingProps>(), {
+    level: 1,
+    family: TextFamily.mono,
+})
+// Compute
+const headingTag = computed(() => `h${props.level}`);
+const classNames = computed(() => [props.size, props.family, props.weight].filter(Boolean).join(' '))
+</script>
+
+<template>
+    <component :is="headingTag" class="text-gray-50" :class="classNames">
+        <slot></slot>
+    </component>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ resources/themes/new/scripts/components/atoms/typography/Text.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import {TextFamily, TextSize} from "../../../domain/types/components";
+import {computed} from "vue";
+
+interface ParagraphProps {
+    size?: TextSize
+    family?: TextFamily
+    component?: 'p' | 'div' | 'span'
+}
+
+const props = withDefaults(defineProps<ParagraphProps>(), {
+    component: "p",
+    family: TextFamily.mono,
+})
+
+const classNames = computed(() => [props.size, props.family].filter(Boolean).join(' '))
+</script>
+
+<template>
+    <component :is="component" class="text-green-50" :class="classNames"><slot></slot></component>
+</template>
+
+<style scoped lang="scss">
+
+</style>
