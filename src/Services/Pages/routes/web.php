9f01411808a3c4fc77cad61f5246491bb1a3d1eb
ilya.shabanov <ishabanov@liquid-cats.com>
2024-07-14 15:38:34 +0400

    chore(new theme): add new layout render

--- src/Services/Pages/routes/web.php
+++ src/Services/Pages/routes/web.php
@@ -6,16 +6,20 @@ use App\Pages\Presentation\Http\Controllers\PostController;
 use App\Pages\Presentation\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', HomepageController::class)->name('pages.home');
-Route::get('/about', AboutController::class)->name('pages.about');
+Route::get('/{frontend?}', static fn () => view('layouts.default'))
+    ->where('frontend', '.*')
+    ->name('pages.home');
 
-Route::prefix('blog')
-    ->group(static function () {
-        Route::get('/', PostsController::class)->name('pages.blog');
-        Route::get('{post_id}', PostController::class)->name('pages.blog.post');
-    });
-Route::prefix('posts')
-    ->group(static function () {
-        Route::get('/', PostsController::class)->name('pages.blog');
-        Route::get('{post_id}', PostController::class)->name('pages.blog.post');
-    });
+// Route::get('/', HomepageController::class)->name('pages.home');
+// Route::get('/about', AboutController::class)->name('pages.about');
+//
+// Route::prefix('blog')
+//     ->group(static function () {
+//         Route::get('/', PostsController::class)->name('pages.blog');
+//         Route::get('{post_id}', PostController::class)->name('pages.blog.post');
+//     });
+// Route::prefix('posts')
+//     ->group(static function () {
+//         Route::get('/', PostsController::class)->name('pages.blog');
+//         Route::get('{post_id}', PostController::class)->name('pages.blog.post');
+//     });
