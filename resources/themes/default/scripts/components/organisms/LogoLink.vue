c5995cb8d57b00e8aa7e9f06cb51c75feb00ff4e
ilya.shabanov <ishabanov@liquid-cats.com>
2024-07-26 23:56:51 +0400

    chore(frontend): app logo

---
+++ resources/themes/default/scripts/components/organisms/LogoLink.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+//
+import logoSvg from "../../assets/images/logo.svg";
+import AppLink from "../atoms/AppLink.vue";
+import {RouteNames} from "../../domain/enums/routes";
+
+interface Props {
+    size: 6|8
+}
+
+// Define
+defineProps<Props>()
+</script>
+
+<template>
+    <AppLink :to="{name: RouteNames.HOMEPAGE}">
+        <img :src="logoSvg" alt="logo"  class="rounded" :class="{
+            'size-6': size == 6,
+            'size-8': size == 8,
+        }">
+    </AppLink>
+</template>
