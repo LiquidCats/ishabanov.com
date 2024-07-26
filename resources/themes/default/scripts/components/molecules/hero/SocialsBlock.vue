29e322d40bf1b720b47e18bdc00e0abcaf963f5c
ilya.shabanov <ishabanov@liquid-cats.com>
2024-07-26 23:55:25 +0400

    chore(frontend): use universal app link component

--- resources/themes/default/scripts/components/molecules/hero/SocialsBlock.vue
+++ resources/themes/default/scripts/components/molecules/hero/SocialsBlock.vue
@@ -1,27 +1,26 @@
 <script setup lang="ts">
 
-import {ArrowUpRightIcon} from "@heroicons/vue/24/outline";
-import Link from "../../atoms/Link.vue";
+import AppLink from "../../atoms/AppLink.vue";
 import type {Link as LinkType} from "../../../domain/types/link";
 
 interface Props {
-    socials: Array<LinkType>
+    item: LinkType
 }
 // Define
 defineProps<Props>()
 </script>
 
 <template>
-    <div v-for="item in socials" class="bg-gradient-to-tl from-blue-500 to-purple-500 rounded-md justify-center flex items-center relative">
-        <Link :href="item.link"
+    <div class="bg-gradient-to-tl from-blue-600 to-purple-400 rounded-md justify-center flex items-center relative">
+        <AppLink :to="item.link"
               :aria-label="item.text"
               :title="item.text"
               target="_blank"
-              class="py-3 duration-300 flex flex-col items-center justify-center text-sm group/contact">
+              class="py-3 duration-300 flex flex-col items-center justify-center text-sm group/contact hover:!text-gray-50">
             <div class="absolute -inset-1" />
-            <ArrowUpRightIcon class="size-10 md:duration-300 md:translate-y-3.5 md:group-hover/contact:translate-y-0" />
-            <span class="md:duration-300 md:opacity-0 md:-translate-y-3.5 md:group-hover/contact:translate-y-0 md:group-hover/contact:opacity-100 motion-reduce:transition-none">{{ item.text }}</span>
-        </Link>
+            <component :is="item.icon" class="size-10 md:duration-300 md:translate-y-1 md:group-hover/contact:-translate-y-1"/>
+            <span class="md:duration-300 md:opacity-75 md:translate-y-1 md:group-hover/contact:translate-y-0 md:group-hover/contact:opacity-100 motion-reduce:transition-none">{{ item.text }}</span>
+        </AppLink>
     </div>
 </template>
 
