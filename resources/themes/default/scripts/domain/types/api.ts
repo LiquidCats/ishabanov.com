85ad9d9519e291823c6679ca9f088e75e1e189cb
ilya.shabanov <ishabanov@liquid-cats.com>
2024-07-26 23:53:54 +0400

    chore(frontend): add api resources types

---
+++ resources/themes/default/scripts/domain/types/api.ts
@@ -0,0 +1,85 @@
+import {ResponseMetaLink} from "../../../../../admin/scripts/types/api";
+import {PreviewTypeEnum} from "../enums/preview";
+import {ToolLevel, ToolType} from "../enums/tool";
+
+export interface ExperienceResource {
+    id: number,
+    company_url: string,
+    company_logo: string,
+    position: string,
+    company_name: string,
+    tools: Array<ToolResource>,
+    description: Array<string>,
+    started_at: string,
+    ended_at: string,
+}
+
+export interface ToolResource {
+    readonly id: string
+    readonly name: string
+    readonly type: ToolType
+    readonly level: ToolLevel
+}
+
+export interface TagResource {
+    readonly id: number
+    readonly name: string
+    readonly slug: string
+}
+
+export interface PostResource {
+    readonly id: number
+    readonly title: string
+    readonly preview: string
+    readonly is_draft: boolean
+    readonly published_at: string
+    readonly reading_time: number
+    readonly preview_image_type: PreviewTypeEnum
+    readonly preview_image_id: number
+    readonly blocks: []
+    readonly tags: Array<TagResource>
+    readonly previewImage: FileResource
+}
+
+export interface FileResource {
+    readonly hash: string
+    readonly type: string
+    readonly path: string
+    readonly extension: string
+    readonly name: string
+    readonly file_size: number
+}
+
+export interface ApiError<T = undefined> {
+    readonly data?: T
+    readonly message: string
+    readonly status: "success"|"fail"|"error"
+}
+
+export type ValidationErrors = {
+    readonly [key: string]: string[]
+}
+
+export interface APIResponse<T extends any> {
+    readonly data: T
+    readonly links: ResponseLinks
+    readonly meta: ResponsePaginationMeta
+}
+
+export interface ResponseLinks {
+    readonly first?: string
+    readonly last?: string
+    readonly prev?: string
+    readonly next?: string
+}
+
+export interface ResponsePaginationMeta {
+    readonly current_page: number,
+    readonly from: number,
+    readonly last_page: number,
+    readonly links: ResponseMetaLink[],
+    readonly path: string,
+    readonly per_page: number,
+    readonly to: number,
+    readonly total: number
+}
