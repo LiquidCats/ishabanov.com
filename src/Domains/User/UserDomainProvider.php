6a24d6edc7e8ccab5b75e630559e3e2713e3b7ac
Ilya Shabanov <ishabanov@liquid-cats.com>
2024-11-16 19:16:52 +0400

    chore: refactor

--- src/Domains/User/UserDomainProvider.php
+++ src/Domains/User/UserDomainProvider.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace App\Domains\User;
 
+use App\Data\Database\Eloquent\Models\UserModel;
+use App\Domains\User\Contracts\Repositories\UserRepositoryContract;
+use App\Domains\User\Contracts\Services\UserServiceContract;
+use App\Domains\User\Services\UserService;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 
@@ -12,5 +16,12 @@ class UserDomainProvider extends ServiceProvider
     public function boot(): void
     {
         Route::pattern('user_id', '[0-9]{1,32}');
+    }
+
+    public function register(): void
+    {
+        $this->app->bind(UserRepositoryContract::class, UserModel::class);
+
+        $this->app->singleton(UserServiceContract::class, UserService::class);
     }
 }
