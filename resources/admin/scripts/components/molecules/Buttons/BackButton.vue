6856aea6dc208aa33c5fd324208cbb4abaf3fbd6
Ilya Shabanov <ishabanov@liquid-cats.com>
2024-11-17 14:58:36 +0400

    chore(): specialized buttons

---
+++ resources/admin/scripts/components/molecules/Buttons/BackButton.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+import Btn from "../../atoms/Btn.vue";
+import {ArrowLeftIcon} from "@heroicons/vue/20/solid";
+import {useRouter} from "vue-router";
+
+const router = useRouter()
+
+</script>
+
+<template>
+    <Btn type="light" @click="router.back()">
+        <ArrowLeftIcon class="size-6"/> Back
+    </Btn>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ resources/admin/scripts/components/molecules/Buttons/SaveButton.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import Btn from "../../atoms/Btn.vue";
+import {ArrowDownOnSquareIcon} from "@heroicons/vue/20/solid";
+
+</script>
+
+<template>
+    <Btn type="primary">
+        <ArrowDownOnSquareIcon class="size-6" /> Save
+    </Btn>
+</template>
+
+<style scoped lang="scss">
+
+</style>
