059805e8d655a55f39483276694484329e6f17ab
Ilia Shabanov <ishabanov@liquid-cats.com>
2025-07-01 00:38:28 +0400

    chore: update atoms

--- components/atoms/Tag.vue
+++ components/atoms/Tag.vue
@@ -1,11 +1,8 @@
 <script setup lang="ts">
-
-import {Hash} from "lucide-vue-next";
-
+import { Hash } from "lucide-vue-next";
+import { Badge } from "@/components/ui/badge";
 </script>
 
 <template>
-    <span class="inline-flex items-center flex-nowrap text-nowrap justify-center bg-gray-100 text-stone-800 py-0.5 px-1.5 rounded-xs text-sm">
-        <Hash class="size-4"/><slot/>
-    </span>
+  <Badge variant="secondary"><Hash class="size-4" /><slot /> </Badge>
 </template>
